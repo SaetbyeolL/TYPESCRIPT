9dd806c226941ae67299643e0cc1ec7d3505a1bd
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-06-24 00:01:48 -0400

    unknown, never, void

--- BasicTypes.ts
+++ BasicTypes.ts
@@ -7,13 +7,13 @@ let c : boolean[] = [true]
 //How to explicitly write the type of an object
 type Age = number;
 type Player =  {   // Type Alias
-    name: string,
+    readonly name: string,
     age?: Age      // optional parameter
 }
-const player : Player = {
+const nico : Player = {
     name: "nico"
 }
-const playerLynn: Player = {
+const Lynn: Player = {
     name: "lynn",
     age: 11    
 }
@@ -21,30 +21,63 @@ const playerLynn: Player = {
 //return type
 function playerMaker(name: string) : Player{ // How to write the type of argument
     return {
-        name
+        name: name
     }
 }
 // same as above::  const playerMaker = (name: string) : Player => ({name})
-const nico = playerMaker("nico")
-nico.age = 12
-
-//////////////////////////////////////////////////////////////////////////////////////////
+const nicho = playerMaker("nicho")
+nicho.age = 12
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ReadOnly : this cannot be modified
+type Player =  {   
+    readonly name: string,
+    age?: number      
+}
+
+const numbers: readonly number[] = [1,2,3,4]
+numbers.push(1)                // this will cause error
 
 
+// Tuple: specify each element types and length of the array clearly
+const player: [string, number, boolean] = ["nico", 12, false]
+player[0] = "stella"
+
+// undefinedL A value automatically assigned to variables, properties, function return values, etc., 
+// that have not been assigned a value.
+let a : undefined = undefined // optionalType(?) can be 'undefined'
+let b : null = null           // no value
+
+// any: escape from typescript protection. not recommended
+const c : any[] = [1,2,3,4]
+const d: any = true
+c + d                         // it does not cause error
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// unknown: When the type of a variable is not known in advance
+let a: unknown;
+if (typeof a === 'number') {
+    let b = a + 1
+}
 
+if(typeof a === 'string') {
+    let b = a.toUpperCase();
+}
 
+// void: return nothing
+function hello() {
+    console.log('x')
+}
+const c = hello();
 
-
-
-
-
-
-
+// never: When a function never returns, throw error, infinite loop
+function Hi(): never{
+    throw new Error("xxx")
+}
 
 
 
