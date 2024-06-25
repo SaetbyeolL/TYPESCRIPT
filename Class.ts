a4ae6cb215da4eec982cd942c0ad287e51e8d4d4
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-06-24 20:24:32 -0400

    class, abstract, protected&private&public

---
+++ Class.ts
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// private 
+
+class Player {
+    constructor(
+        private firstName: string,  // private only allowed in Typescript
+        private lastName: string,
+        public nickname: string
+    ){}
+}
+
+const nico = new Player("nico", "las", "nick");
+nico.firstName // it does not allows to modify 'firstName'
+
+
+// Abstract class: This class cannot create new instance directly
+//                 This can only be used when you inherit abstract class to subclass
+abstract class User {
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        public nickname: string
+    ) {}
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+
+//* child class cannot have 'private' properties and methods
+class Player extends User { // Player inherits User
+}
+const niko = new Player("nico", "las", "nick")
+niko.getFullName()
+
+
+// Abstract method: abstract method is a method that must be implemented by all classes that inherit from the abstract class.
+// protected: it allows within inside of declared class, subclass
+// public: default
+// private: only within class 
+
+abstract class User {
+    constructor(
+        protected firstName: string,
+        protected lastName: string,
+        protected nickname: string
+    ) {}
+
+    abstract getNickName(): void //this class cannot be instantiated
+
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+ 
+class Player extends User {
+    getNickName(){
+
+        
+    }
+}
+const niko = new Player("nico", "las", "nick")
+niko.getFullName()
+niko.firstName // it cause error
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Creating Dictionary using class
+type Words = {
+    [key:string]: string
+}
+
+class Dictionary {
+    private words: Words
+    
+    constructor(){
+        this.words = {}
+    }
+    add(word: Word){
+        if(this.words[word.term] === undefined){
+            this.words[word.term] = word.def;
+        }
+    }
+    def(term: string){
+        return this.words[term]
+    }
+}
+
+class Word {
+    constructor(
+        public term: string, 
+        public def: string
+    ) {}
+}
+
+const kimchi = new Word("kimchi", "korean food")
+const dict = new Dictionary()
+dict.add(kimchi)
+dict.def("kimchi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
